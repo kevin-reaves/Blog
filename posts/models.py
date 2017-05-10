6409083f3e391046391f582b02d0cda9e5f3b5cc
= <kevin82593@hotmail.com>
2017-05-09 21:31:39 -0500

    blog

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from tinymce.models import HTMLField
 
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=250)
     pub_date = models.DateTimeField()
     image = models.ImageField(upload_to='media/')
     body = HTMLField()
+
+    def __str__(self):
+        print(self.pub_date)
+        return str(self.title + "  " + self.pub_date.strftime("%Y-%m-%d"))
